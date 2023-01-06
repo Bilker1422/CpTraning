af3e20a4eedb0af6a8753a49a0ba465be1e89a6f
Bilker1422 <bilker1422@gmail.com>
2023-01-06 19:15:23 +0300

    .

---
+++ 1328A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    cin>> n;
+    while(n--){
+        int x,y;
+        cin >> x >> y;
+        cout <<((y-x%y)%y) <<"\n";
+    }
+}
+
+
